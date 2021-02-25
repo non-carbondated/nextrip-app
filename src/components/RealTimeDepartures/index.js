d74a1f43e6a7e0eba919ba8d8f7d6a0c60264a17
Matthew Meyer <ner_nu_anar@yahoo.com>
2021-02-24 19:36:56 -0600

    feat(RealTimeDepartures): add business logic for routes data Add query for routes data. Add change handler for selected route.

--- src/components/RealTimeDepartures/index.js
+++ src/components/RealTimeDepartures/index.js
@@ -1,3 +1,29 @@
-import RealTimeDepartures from './RealTimeDepartures'
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import { getActiveRoutes } from './RealTimeDepartures.api'
+import RealTimeDeparturesUI from './RealTimeDepartures'
+
+const RealTimeDepartures = () => {
+  const [selectedRouteId, setSelectedRouteId] = useState(null)
+
+  const routes = useQuery('routes', getActiveRoutes)
+
+  const handleRouteChange = id => setSelectedRouteId(id)
+
+  return (
+    <RealTimeDeparturesUI
+      selectedRouteId={selectedRouteId}
+      selectedDirectionId={null}
+      selectedPlaceCode={null}
+      routes={routes.data}
+      directions={[]}
+      stops={[]}
+      realTimeDepartures={[]}
+      onRouteChange={handleRouteChange}
+      onDirectionChange={() => {}}
+      onStopChange={() => {}}
+    />
+  )
+}
 
 export default RealTimeDepartures

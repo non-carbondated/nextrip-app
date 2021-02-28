06d79207c8df772e072d3eb49a2fbb3ff8324846
Matthew Meyer <ner_nu_anar@yahoo.com>
2021-02-28 14:17:38 -0600

    test(App): add App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders navigation buttons', () => {
+  render(<App />)
+
+  const realTimeDeparturesButton = screen.getByRole('button', { name: 'Real time departures' })
+  const aboutButton = screen.getByRole('button', { name: 'About' })
+
+  expect(realTimeDeparturesButton).toBeInTheDocument()
+  expect(aboutButton).toBeInTheDocument()
+})
+
+test('renders the Real time departures page by default', () => {
+  render(<App />)
+
+  expect(screen.getByRole('heading', { name: 'Real time departures' })).toBeInTheDocument()
+})
+
+test('renders the About page when the About navigation button is clicked', () => {
+  render(<App />)
+
+  userEvent.click(screen.getByRole('button', { name: 'About' }))
+
+  expect(screen.getByRole('heading', { name: 'About this project' })).toBeInTheDocument()
+})
+
+test('renders the Real time departures page when the Real time departures navigation button is clicked', () => {
+  render(<App />)
+
+  userEvent.click(screen.getByRole('button', { name: 'Real time departures' }))
+
+  expect(screen.getByRole('heading', { name: 'Real time departures' })).toBeInTheDocument()
+})
